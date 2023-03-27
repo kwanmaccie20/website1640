351022a235f4f0730caa9ca46fffb698d2d759c6
V Khang Nguyen <nguyenvikhang20@gmail.com>
2023-03-27 12:42:38 +0700

    20230327124216.local.DT-KWAN  - [Create auth folder]

---
+++ src/pages/auth/index.js
@@ -0,0 +1,12 @@
+import { Text } from '@mantine/core';
+import Image from 'next/image';
+import React from 'react'
+
+export default function index() {
+  return (
+    <>
+    <Text>Welcome</Text>
+    <Image></Image>
+    </>
+  )
+}
